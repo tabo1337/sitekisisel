8f5ec0507dcfa2de3e4727f7d45040c8fd87c343
tabo1337 <140086724+tabo1337@users.noreply.github.com>
2024-01-31 18:46:40 +0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header className="App-header">
+            <p><a href="https://github.com/Berikai">
+            <img 
+                className="pp_logo" 
+                src="https://avatars.githubusercontent.com/u/18515671?v=4" 
+                alt="Berikai" />
+            </a></p>
+            <div className="username">Berikai<div className="aka">also known as Verdant</div></div>
+        </header>
+    )
+}
